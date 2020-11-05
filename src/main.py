b464a00cd7e17def582bdc0bf473094981bdc66e
Vyvy-vi <vyom.j@protonmail.com>
2020-11-05 23:49:53 +0530

    feat: added bot structure

--- src/main.py
+++ src/main.py
@@ -6,3 +6,18 @@ from util import get_environment_variable
 
 TOKEN = get_environment_variable("TOKEN")
 MONGO = get_environment_variable("MONGO_URI")
+PREFIX = "!py"
+
+intents = discord.Intents(messages= True, guilds= True)
+bot = commands.Bot(command_prefix = PREFIX, intents=intents)
+
+cog_list = ['cogs.config']
+
+
+
+
+if __name__ == '__main__':
+    for extension in ls_cog:
+        bot.load_extension(extension)
+
+bot.run(TOKEN)
